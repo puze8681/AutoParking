e957fd3499eea69347c933d9a4e82b206ae0a489
puze8681 <puze8681@gmail.com>
2020-06-21 03:39:49 +0900

    date

--- app/src/main/java/kr/puze/autoparking/PrefUtil.kt
+++ app/src/main/java/kr/puze/autoparking/PrefUtil.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import android.content.SharedPreferences
 
 class PrefUtil(context: Context, date: String) {
+    var date = date
     private var preferences: SharedPreferences = context.getSharedPreferences("Price", Context.MODE_PRIVATE)
     private var editor: SharedPreferences.Editor = preferences.edit()
 
     var todayPrice: Int
-        get() = preferences.getInt("date", 0)
+        get() = preferences.getInt(date, 0)
         set(value) {
-            editor.putInt("date", value)
+            editor.putInt(date, value)
             editor.apply()
         }
 }
